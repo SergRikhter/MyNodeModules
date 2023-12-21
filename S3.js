efc954b70d5e6ccd04d41369633b74980eccd107
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-12-21 05:17:34 +0200

    Adding new functionalities . Part 1

--- S3.js
+++ S3.js
@@ -32,6 +32,28 @@ const bucketPolicy = {
 const s3 = new AWS.S3();
 
 /**
+ * Bucket object policy
+ * Политика bucket объекта
+ */
+
+const bucketPolicy = {
+    Bucket: 'mbox-userstorage',
+    Policy: JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [
+            {
+                Effect: 'Allow',
+                Principal: '*',
+                Action: 's3:GetObject',
+                Resource: `arn:aws:s3:::mbox-userstorage/*`
+            }
+        ]
+    })
+};
+
+const s3 = new AWS.S3();
+
+/**
  * Set bucket object policy
  * Установка политики для Bucket объекта
  */
@@ -66,6 +88,52 @@ const selectFile = async (filename) =>{
 }
 
 /**
+ * Create new bucket
+ * Создание новой корзины
+ */
+
+const createBucket = (bucketName) =>{
+ if(bucketName)
+ {
+   s3.createBucket({Bucket: bucketName }, (err,data)=>{
+      err ? console.error("Ошибка создания корзины S3" + err) : console.log("Корзина успешно создана: "+ data);
+   })
+ }else{
+    console.error("Empty bucket name");
+ }
+}
+
+/**
+ * Выводит содержимое объекта
+ * List object
+ * @param {*} object 
+ */
+const forEachObject =(object) =>{
+    if(object){
+        Object.entries(object).forEach(items => {
+            console.log(items);
+          });  
+    }else{
+        console.error("Empty params");
+    }
+}
+
+/**
+ * Displaying the contents of the current bucket
+ * Выводит содержимое текущей корзины 
+ * @param {*} bucketName 
+ */
+const listObject = async (bucketName) =>{
+    if(bucketName){
+      s3.listObjects({Bucket: bucketName}, (err,data)=>{
+            err ? console.log("I can't listing object , because " + err ) : forEachObject(data);
+        })
+    }else{
+        console.error("Empty bucket name");
+    }
+}
+
+/**
  * Загрузка файлов
  * Load files 
  * @param {*} filename 
@@ -74,7 +142,7 @@ const uploadFile = async (filename) => {
     try {
         if(await selectFile(filename) !== false)
         {
-            s3.upload(await selectFile(filename), (err, data) => {
+            s3.upload(file, (err, data) => {
                 err ?  console.error('Ошибка при загрузке файла на S3:', err) :  console.log('Файл успешно загружен на S3. Детали:', data);
             });
             await setPolicy();
@@ -87,4 +155,7 @@ const uploadFile = async (filename) => {
 };
 
 //await uploadFile(); -- Ошибка
-await uploadFile('src/example.txt');
+//await uploadFile('src/example.txt');
+//createBucket("mbox666");
+//await listObject("mbox-userstorage")
+
