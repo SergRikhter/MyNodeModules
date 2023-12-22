ded5fbd90c49638e7cfae868f362830122a22c6a
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-12-22 04:11:56 +0200

    Common fix functions

--- S3.js
+++ S3.js
@@ -1,21 +1,21 @@
 import AWS from 'aws-sdk';
-import fs from 'fs/promises';
+import aws4 from 'aws4';
+import fs from 'fs';
 
 let file={}; 
-AWS.config.update({
-    accessKeyId: 'AccessKeyId',
-    secretAccessKey: 'SecretAccessKeyId',
-    region: 'region',
-    signatureVersion: 'v4'
-});
 
-const s3 = new AWS.S3();
+AWS.config.update({
+    accessKeyId: ' ',
+    secretAccessKey: ' ',
+    region: ' ',
+    ACL: ' ',
+    signatureVersion: ' '
+});
 
 /**
  * Bucket object policy
  * Политика bucket объекта
  */
-
 const bucketPolicy = {
     Bucket: 'mbox-userstorage',
     Policy: JSON.stringify({
@@ -25,11 +25,13 @@ const bucketPolicy = {
                 Effect: 'Allow',
                 Principal: '*',
                 Action: 's3:GetObject',
-                Resource: `arn:aws:s3:::mbox-userstorage/*`
+                Resource: [`arn:aws:s3:::mbox-userstorage/*`,`arn:aws:s3:::mbox-userstorage`],
             }
         ]
     })
 };
+
+const s3 = new AWS.S3();
 
 /**
  * Set bucket object policy
@@ -50,17 +52,42 @@ const setPolicy = async () =>{
  * @param {*} filename 
  * @returns 
  */
-const selectFile = async (filename) =>{
+const selectFile = async (filename, folder,options) =>{
     if(filename)
     {
-        file={
-            Body : await fs.readFile(filename),
-            Bucket: "mbox-userstorage",
-            Key:filename === undefined ?  undefined : filename
+        switch(options)
+        {
+           case "text":
+            file={
+                Body : await fs.readFileSync(filename),
+                Bucket: "mbox-userstorage",
+                Key:filename === undefined ?  undefined : folder+"/"+filename
+            }
+            break;
+            case "image":
+            file={
+                Body : await fs.createReadStream(filename),
+                Bucket: "mbox-userstorage",
+                Key:filename === undefined ?  undefined : folder+"/"+filename,
+                ContentType: 'image/jpeg'
+            }
+            break;  
+            case "music":
+                /*
+            file={
+                Body : await fs.createReadStream(filename),
+                Bucket: "mbox-userstorage",
+                Key:filename === undefined ?  undefined : folder+"/"+filename
+            }*/
+            break;
+            default:
+                return "Error! Incorrect filetype";
+            break;
         }
+       
         return file;
     }else{
-          console.error("File is missing");
+          console.error(" select File:: File is missing");
         return false;
     }
 }
@@ -77,7 +104,7 @@ const createBucket = (bucketName) =>{
       err ? console.error("Ошибка создания корзины S3" + err) : console.log("Корзина успешно создана: "+ data);
    })
  }else{
-    console.error("Empty bucket name");
+    console.error("createBucket :: Empty bucket name");
  }
 }
 
@@ -92,7 +119,7 @@ const forEachObject =(object) =>{
             console.log(items);
           });  
     }else{
-        console.error("Empty params");
+        console.error("forEachObject :: Empty params");
     }
 }
 
@@ -104,36 +131,68 @@ const forEachObject =(object) =>{
 const listObject = async (bucketName) =>{
     if(bucketName){
       s3.listObjects({Bucket: bucketName}, (err,data)=>{
-            err ? console.log("I can't listing object , because " + err ) : forEachObject(data);
+         err ? console.log("I can't listing object , because " + err ) : forEachObject(data);
         })
     }else{
-        console.error("Empty bucket name");
+         console.error("listObject::Empty bucket name");
     }
 }
 
 /**
- * Загрузка файлов
+ * Загрузка текстового файла 
  * Load files 
  * @param {*} filename 
  */
-const uploadFile = async (filename) => {
+const uploadFile = async (filename, location, fileType) => {
     try {
-        if(await selectFile(filename) !== false)
+        const currentFile = await selectFile(filename);
+        if(await selectFile(filename,location,fileType) !== false)
         {
             s3.upload(file, (err, data) => {
-                err ?  console.error('Ошибка при загрузке файла на S3:', err) :  console.log('Файл успешно загружен на S3. Детали:', data);
+                err ?  console.error('Ошибка при загрузке файла на S3:', err) :  console.log('Файл успешно загружен на S3. Детали:', data );
             });
-            await setPolicy();
+           //  await setPolicy();
+         // await readHeadObject();
         }else{
-            console.error("File missing! Abort ...")
+            console.error("uploadFile :: File missing! Abort ...")
         }
     } catch (error) {
         console.error('Ошибка при чтении файла:', error);
     }
 };
 
+const deleteFileStorage = async (filename, location, fileType) =>{
+    try{
+        /*
+        s3.update(await selectFile(filename,location,fileType), (data,err)=>{
+            err ? console.error("UpdateFileStorage:: Storage can't updated") : console.log("UpdateFileStorage::Storage updated successfull!");
+        }); */
+    }catch(err){
+        console.error('UpdateFileStorage:: ' + err);
+    }
+}
+
+const copyFileStorage = async (from, to)=>{
+
+};
+
+const deleteBucket = async(bucketName)=>{
+
+};
+
+/*
+const readHeadObject = async (filename) =>{
+  try{
+    s3.headObject({Bucket:'mbox-userstorage', Key:'1.jpg'}, (data)=>{
+         console.log("Head object:" + data);
+    })
+  }catch(err){
+    console.error("Image didn't loaded , because"+err);
+  }
+}
+*/
 //await uploadFile(); -- Ошибка
-//await uploadFile('src/example.txt');
+//await uploadFile('src/5.png',"User_Images/Sergey", "image");
+//await updateFileStorage('src/2.jpg',"User_Images/Sergey", "image");
 //createBucket("mbox666");
 //await listObject("mbox-userstorage")
-
