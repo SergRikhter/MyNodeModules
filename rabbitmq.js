03b2a9a3bd3138e55f6bc96a0a7e90f72bd390a2
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2025-04-17 00:28:25 +0300

    fix: refactor connection check into a reusable function

--- rabbitmq.js
+++ rabbitmq.js
@@ -3,28 +3,35 @@ import rabbit from 'amqplib';
 import {v4 } from 'uuid';
 dotenv.config();
 
-let connection =0; 
+let connection = 0; 
 let channel = 0;
 let connectString = process.env.CONNECTION_URI_RABITMQ || 'amqp://user:password@rabbitmq:5672';
 let queue = "queue_event";
 
+function checkExistConnection() 
+{
+    return connection || channel ? true : false; 
+}
+
 export async function RabbitMQConnection() 
 {
-    try{
-         connection = await rabbit.connect(connectString);
-         channel= await connection.createChannel();
-         console.log(`[${new Date().toLocaleString()}]   : RabbitMQ Module : Connection successfully`);
+    if(!checkExistConnection())
+    {
+         try{
+             connection = await rabbit.connect(connectString);
+             channel= await connection.createChannel();
+             console.log(`[${new Date().toLocaleString()}]   : RabbitMQ Module : Connection successfully`);
          return true;
-    }catch(err){
+       }catch(err){
           console.error(`[${new Date().toLocaleString()}] : RabbitMQ Module : ${err} `);
          return false;
+       }   
     }
 }
+
 export async function SendQuery(redisKey,email, template,username)
 {
     try{
-         connection = await rabbit.connect(connectString);
-         channel= await connection.createChannel();
         await channel.assertQueue(queue, { durable: false });
         const message = {
             To: `${email}`,
@@ -36,6 +43,6 @@ export async function SendQuery(redisKey,email, template,username)
           };
         await channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
     }catch(err){
-        console.log(`[${new Date().toLocaleString()}] : RabbitMQ Module : ${err} `);
+        console.error(`[${new Date().toLocaleString()}] : RabbitMQ Module : ${err} `);
     }
 }
